a4c86abbae779d97b8286edb67a5eaab883a7e4f
Richard Seroter <richardseroter@Richards-MacBook-Pro.local>
2020-02-24 10:45:10 -0800

    Added REST controller

--- src/main/java/com/seroter/demo/BootSimpleK8sAppApplication.java
+++ src/main/java/com/seroter/demo/BootSimpleK8sAppApplication.java
@@ -2,12 +2,23 @@ package com.seroter.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BootSimpleK8sAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootSimpleK8sAppApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String[] GetTickerSymbols() {
+		
+		String[] symbols = {"VMW", "PVTL"};
+		
+		return symbols;
 	}
 
 }
