9a00ad4e5689040208bc5c227f8a0ef720fe37eb
Richard Seroter <richardseroter@Richards-MacBook-Pro.local>
2020-02-24 10:42:47 -0800

    Initial commit

---
+++ src/main/java/com/seroter/demo/BootSimpleK8sAppApplication.java
@@ -0,0 +1,13 @@
+package com.seroter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSimpleK8sAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSimpleK8sAppApplication.class, args);
+	}
+
+}
